9eab62488978b994369e8fabbfb02979acf8a813
laurahansen <lauramhh10@gmail.com>
2024-12-18 17:53:40 +0100

    update

---
+++ rema.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="rema.css"
+</head>
+<body>
+<nav>
+    <ul>
+        <a href="index.php">
+            <img src="billeder/nytlogo.png" class="logo">
+        </a>
+        <li>
+            <a href="#">Madspild</a>
+            <ul class="dropdown">
+                <li><a href="tipsogtricks.php">Tips og tricks</a></li>
+                <li><a href="supermarkeder.php">Supermarkeder</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Retter og måltider</a>
+            <ul class="dropdown">
+                <li><a href="madgenerator.php">Madgenerator</a></li>
+                <li><a href="#">Madplaner</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Donationsbank</a></li>
+        <li><a href="#">Madpyramiden</a></li>
+    </ul>
+</nav>
+
+<section class="bjælke1">
+</section>
+
+<section class="rema">
+    <img src="billeder/Rema_logo.svg.png" class="rema">
+</section>
+
+
+<div class="Overskrift">
+
+    <h1>Som en af de førende supermarkedskæder i Danmark tager REMA 1000 kampen mod madspild seriøst og har udviklet initiativer, der passer perfekt til unge studerende, der ønsker at leve mere bæredygtigt og spare penge.</h1>
+</div>
+
+
+<div class="overskrift">
+
+    <h2>Reduceret priser</h2>
+
+</div>
+
+<div class="information">I REMA 1000 kan du finde varer tæt på udløbsdato til stærkt reducerede priser.
+    Det gør det lettere for dig at få kvalitetsfødevarer til en billigere pris, samtidig med at du hjælper med at redde mad fra at blive smidt ud.</div>
+
+
+<section class="klistermærke">
+    <div class="klistermærke">
+        <img src="billeder/klistermærke.png" class="klistermærke">
+        <section class="klistermærke"></section>
+    </div>
+</section>
+
+<section class="klistermærke2">
+    <div class="klistermærke2">
+        <img src="billeder/klistermærke2.png" class="klistermærke2">
+        <section class="klistermærke2"></section>
+    </div>
+</section>
+
+<div class="overskrift2">Støtter “Stop spild af mad”</div>
+
+<div class="information2">REMA 1000 støtter organisationen Stop Spild Af Mad og arbejder på at gøre en forskel i samfundet ved at bekæmpe madspild både i deres butikker og hos kunderne.</div>
+
+
+<section class="klistermærke3">
+    <div class="klistermærke3">
+        <img src="billeder/klistermærke3.png" class="klistermærke3">
+        <section class="klistermærke3"></section>
+    </div>
+</section>
+
+<div class="overskrift3">Praktiske madspildsråd</div>
+
+<div class="information3">REMA 1000 deler tips og tricks om, hvordan du kan undgå madspild derhjemme. For eksempel ved at planlægge måltider, fryse rester eller genbruge ingredienser i nye retter.</div>
+
+
+<section class="klistermærke4">
+    <div class="klistermærke4">
+        <img src="billeder/klistermærke4.png" class="klistermærke4">
+        <section class="klistermærke4"></section>
+    </div>
+</section>
+
+<div class="overskrift4">Hvorfor er det relevant for dig </div>
+
+<div class="information4">REMA 1000 gør det nemt for unge studerende at leve grønt uden at sprænge budgettet. Ved at købe datovarer eller Too Good To Go-poser kan du spare penge og mindske madspild. Deres fokus på smartere køb og mindre forbrug passer perfekt til en travl og økonomisk hverdag.</div>
+
+<footer>
+    <img src="billeder/køge.png" class="køgelogo">
+    <div class="line">
+        <div></div>
+    </div>
+    <h3>KØGE KOMMUNE</h3>
+    <br>
+    <h4>Køge Kommune <br><br> Torvet 1 <br><br> DK-4600 Køge <br><br> CVR-nr. 29189374</h4>
+    <br>
+    <br>
+    <h6>&copy; Bekæmp Madspild.</h6>
+
+    <div class="følg">
+        <h5 style="background-color: #557D56">FØLG</h5>
+        <img src="billeder/socialemedier.png" class="socialemedier">
+    </div>
+
+    <div class="personligeoplysninger">
+        Håndtering af personlige oplysninger
+    </div>
+    <div class="betingelser">
+        Betingelser for brug
+    </div>
+    <div class="politik">
+        Cookie politik
+    </div>
+    <div class="cookies">
+        Vælg en anden cookies
+    </div>
+</footer>
+</body>
+</html>
